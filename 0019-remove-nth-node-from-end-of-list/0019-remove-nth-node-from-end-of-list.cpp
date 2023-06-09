a2bbf2f5a6a2dbcefddd2b79f53e115dcaa43ec6
Ritesh Gangwar <116111183+RiteshGangwar2022@users.noreply.github.com>
2023-06-09 17:34:44 +0530

    Time: 3 ms (81.64%), Space: 10.7 MB (54.93%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -11,7 +11,7 @@
 class Solution {
 public:
     
-    int len(ListNode*head){
+    /*int len(ListNode*head){
         
         int count=0;
         while(head!=NULL){
@@ -20,10 +20,12 @@ public:
         head=head->next;
         }
         return count;
-    }
+    }*/
     ListNode* removeNthFromEnd(ListNode* head, int n) {
         
-           int size=len(head);
+        
+        //tc O(size of linked list)
+       /*    int size=len(head);
         
         if(head->next==NULL&&n==1){
             return NULL;
@@ -44,7 +46,34 @@ public:
             i++;
         }
         temp->next=temp->next->next;
-        return head;
+        return head;*/
+        
+        
+        //another efficient appraoch , two pointers algo,(slow,fast)
+        
+        // TC=>O(n)
+        
+        ListNode*start=new ListNode(-1);
+        start->next=head;
+        ListNode*slow=start;
+        ListNode*fast=start;
+        
+        //move fast till n
+        for(int i=0;i<n;i++){
+            fast=fast->next;
+        }
+        
+        //move fast till fast reaches null
+        while(fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next;
+        }
+        
+        //perform remove operation
+        slow->next=slow->next->next;
+        return start->next;
+        
+        
         
     }
 };
