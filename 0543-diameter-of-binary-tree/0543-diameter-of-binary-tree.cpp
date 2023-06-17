bda8187f612277d63350c969e47876e13bb8bf93
Ritesh Gangwar <116111183+RiteshGangwar2022@users.noreply.github.com>
2023-06-17 14:20:37 +0530

    Time: 13 ms (53.19%), Space: 20.7 MB (9.61%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -12,7 +12,7 @@
 class Solution {
 public:
     
-    int height(TreeNode*root){
+   /*int height(TreeNode*root){
           
         if(root==NULL){
 
@@ -20,13 +20,38 @@ public:
         }
         
         return max(height(root->left),height(root->right))+1;
+    }*/
+    
+    pair<int,int> helper(TreeNode*root){
+        
+        if(root==NULL){
+            pair<int,int>ans;
+            ans={0,0};
+            return ans;
+        }
+        
+        pair<int,int>left=helper(root->left);
+        pair<int,int>right=helper(root->right);
+        
+        int ld=left.second;
+        int rd=right.second;
+        
+        int lh=left.first;
+        int rh=right.first;
+        
+         pair<int,int>res;
+         res.first=max(lh,rh)+1;
+         res.second=max(max(ld,rd),lh+rh);
+         return res;
+        
     }
     int diameterOfBinaryTree(TreeNode* root) {
         
         
         //max diamter can be present on left, right and , both side of the binary tree
         //so, will return max of all the three
-        if(root==NULL){
+        //this will take O(n^2) and Sc=>O(N)
+       /* if(root==NULL){
             return 0;
         }
         
@@ -38,7 +63,17 @@ public:
         //getting maxheight from both left and right
          int lh=height(root->left);
          int rh=height(root->right);
-        return max(max(leftdiameter,rightdiameter),lh+rh);
+        return max(max(leftdiameter,rightdiameter),lh+rh);*/
+        
+        //as we are calling two funtion to get the height and traverse to tree
+        //we can store that in pair 
+         
+        //<height,diamter>
+        
+         pair<int,int>ans=helper(root);
+        
+           return ans.second;
+        
         
     }
 };
