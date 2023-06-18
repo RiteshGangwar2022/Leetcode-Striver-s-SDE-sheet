0d4b097bb41e43694c59fd99aa64739d3f6dddee
Ritesh Gangwar <116111183+RiteshGangwar2022@users.noreply.github.com>
2023-06-18 11:10:38 +0530

    Time: 3 ms (99.76%), Space: 22.1 MB (7.15%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -12,7 +12,7 @@
 class Solution {
 public:
     
-    int height(TreeNode*root){
+    /*int height(TreeNode*root){
         
         if(root==NULL){
             return 0;
@@ -20,24 +20,59 @@ public:
         
         return max(height(root->left),height(root->right))+1;
         
+    }*/
+    
+    pair<bool,int> helper(TreeNode*root){
+        
+        if(root==NULL){
+            
+            pair<bool,int>res;
+            res={true,0};
+            return res;
+        }
+        
+        pair<bool,int>left=helper(root->left);
+        pair<bool,int>right=helper(root->right);
+         
+        int lh=left.second;
+        int rh=right.second;
+           bool leftbal=left.first;
+           bool rightbal=right.first;
+            int height=max(lh,rh)+1;
+            
+            pair<bool,int>res;
+             res.second=height;
+            if((abs(lh-rh)<=1)&&rightbal&&leftbal){
+                res.first=true;
+            }
+             else{
+                 res.first=false;
+             }
+            return res;
+                                   
     }
     bool isBalanced(TreeNode* root) {
         
-        if(root==NULL){
+        
+        //TC =>O(n^2) and SC=>O(H)
+       /* if(root==NULL){
             return true;
         }
         
      
         bool left=isBalanced(root->left);
         bool right=isBalanced(root->right);
-           int lh=height(root->left);
-           int rh=height(root->right);
+        int lh=height(root->left);
+        int rh=height(root->right);
         if((abs(lh-rh)<=1)&&left&&right){
              return true;
         }
         else{
             return false;
-        }
+        }*/
+        pair<bool,int>ans=helper(root);    //pair<isbst,height>
+        return ans.first;
+        
         
     }
 };
