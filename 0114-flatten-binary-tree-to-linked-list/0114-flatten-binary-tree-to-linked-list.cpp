7dec1aa7efb6334510ec0eba568180755506c3f2
Ritesh Gangwar <116111183+RiteshGangwar2022@users.noreply.github.com>
2023-06-18 13:09:31 +0530

    Time: 7 ms (62.65%), Space: 12.6 MB (61.03%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -18,8 +18,8 @@ public:
         //but we cannot create new linked list
         
          //using recursion
-        
-        if(root==NULL){
+        //TC =>o(n) and Sc=>O(n)
+      /*  if(root==NULL){
 
             return;
         }
@@ -27,9 +27,55 @@ public:
         
         flatten(root->right);
         flatten(root->left);
+        //make connection to right node
         root->right=prev;
+        //break connection from left node
         root->left=NULL;
-        prev=root;
+        prev=root;//update prev to root*/
+        
+        //iterative solution
+        
+        //take a stack ,push root into it, push roots left and right part , point curr->right to st.top(), 
+        
+        //TC =>O(n) and SC=>O(n)
+        
+       /* stack<TreeNode*>s;
+        s.push(root);
+        TreeNode*curr=root;
+        while(curr!=NULL){
+            
+            curr=st.top();
+            st.pop();
+            if(curr->right){
+                s.push(curr->right);
+            }
+            if(curr->left){
+                s.push(curr->left);
+            }
+            
+            if(s.size()){
+                  curr->right=st.top();
+            }
+            curr->left=NULL;
+        }*/
+        
+        //morris traversal =>   TC =>O(n) and Sc(1)
+        
+        TreeNode*curr=root;
+        while(curr!=NULL){
+            
+            if(curr->left){
+                  
+                TreeNode*prev=curr->left;
+                while(prev->right!=NULL){
+                    prev=prev->right;
+                }
+                prev->right=curr->right;
+                curr->right=curr->left;
+            }
+            curr->left=NULL;
+            curr=curr->right;
+        }
            
     }
 };
