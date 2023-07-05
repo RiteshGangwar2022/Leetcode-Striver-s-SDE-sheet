09296538fb736e5f8ea0915105e96724a22c71ed
Ritesh Gangwar <116111183+RiteshGangwar2022@users.noreply.github.com>
2023-07-05 13:34:07 +0530

    Time: 24 ms (66.66%), Space: 16.4 MB (29.57%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,46 +1,61 @@
 class MinStack {
 public:
-    stack<int>s;
-    stack<int>minimum;
     
+    stack<long long>st;
+    long long mini;
     MinStack() {
-        
-        
         /*
-        first-app=>use two stack , one will store  all the elements and another will store   minimum element.
-        =>we will keep track of minimum element in min stack 
-        //TC =>o(1) and SC =>O(2*n)
+        2nd app=>we will solve it using single loop=> O(1) extra space
+          we will take a variable which will store the minimum element, 
+          if(st.top()>val), then just ,push the element into stack
+          else
+          =>we will use formula => 2*val-st.top(), and push it into stack, this integer will act as a boolean variable , so, whenver we return the current minimum element,
+          we will update our minimum elemenet with = 2*minimum-st.top()
         */
-        
-        return;
     }
-    
-    void push(int val) {
-        
-        s.push(val);
-        if(minimum.size()==0){
-               minimum.push(val);
-        }
-        else if(minimum.top()>=val){
-             minimum.push(val);
-        }
-        return;
+  void push(int value) {
+    long long val = value;
+    if (st.empty()) {
+      mini = val;
+      st.push(val);
+    } else {
+      if (val < mini) {
+        st.push(2 *val*1LL - mini);
+        mini = val;
+      } else {
+        st.push(val);
+      }
     }
-    
-    void pop() {
-        int ans=s.top();
-        s.pop();
-        if(minimum.top()==ans){
-            minimum.pop();
-        }
-        return;
+  }
+
+  void pop() {
+    if (st.empty()) return;
+    long long el = st.top();
+    st.pop();
+
+    if (el < mini) {
+      mini = 2 * mini - el;
     }
-    
-    int top() {
-        return s.top();
-    }
-    
-    int getMin() {
-        return minimum.top();
-    }
+  }
+
+  int top() {
+    if (st.empty()) return -1;
+
+    long long el = st.top();
+    if (el < mini) return mini;
+    return el;
+  }
+
+  int getMin() {
+    return mini;
+  }
 };
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
