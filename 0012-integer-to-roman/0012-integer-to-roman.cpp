7ce70d877125dbce82f2ba86899489a52419d8a0
Ritesh Gangwar <116111183+RiteshGangwar2022@users.noreply.github.com>
2023-07-12 16:57:54 +0530

    Time: 7 ms (77.31%), Space: 8.9 MB (25.09%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -2,15 +2,17 @@ class Solution {
 public:
     string intToRoman(int num) {
        
-        vector<int>number{1,4,5,9,10,40,50,90,100,400,500,900,1000};
+        
+        //TC =>O(N) and SC =>O(N)
+       vector<int>number{1,4,5,9,10,40,50,90,100,400,500,900,1000};
         vector<string>roman{"I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"};
     
     
         string ans="";
-    
+    /*substraction approach
     for(int i=12;i>=0;i--){
         
-        
+          //add string char from roman vector till num is greater than or equal to number vector
            while(num>=number[i]){
                
                ans+=roman[i];
@@ -18,6 +20,22 @@ public:
            }
     }
          
+        return ans;*/
+        
+        
+        //division approach
+         int index=12;
+          
+        while(num>0){
+            
+             int newnum=num/number[index];
+            
+              for(int i=0;i<newnum;i++){
+                   ans+=roman[index];
+              }
+              num=num%number[index];
+              index--;
+        }
         return ans;
         
     }
