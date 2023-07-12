3350ee369880970f045a7e921e2e6d9884ab9b2e
Ritesh Gangwar <116111183+RiteshGangwar2022@users.noreply.github.com>
2023-07-12 14:51:45 +0530

    Time: 2 ms (89.36%), Space: 7.2 MB (79.37%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -2,22 +2,36 @@ class Solution {
 public:
     string reverseWords(string s) {
     
-                  
-          string result="";
-        int n=s.size();
-        int i=0;
-        while(i<n){
-            while(i<n && s[i]==' ') i++;
-            if(i==n) break;
-            int j=i;
-            while(j<n && s[j]!=' ') j++;
-            string str=s.substr(i,j-i);
-            if(result.size()==0) result=str;
-            else result=str+" "+result;
-            i=j+1;
-        }
-        return result;
-          
-
+        
+          string ans="";
+        
+          int i=s.size()-1;
+         while(i>=0){
+                
+               while(i>=0 && s[i]==' '){
+                    i--;             //removing all the trailing spaces
+               }
+                
+                if(i<0){
+                    break;
+                }
+             
+               int  j=i;
+               
+             //now will again move to get word
+               while(i>=0 && s[i]!=' '){
+                   i--;
+               }
+             
+               if(ans.size()==0){
+                     ans+=s.substr(i+1,j-i); //substr(included, excluded) , substr(pos,size)
+               }
+               else{
+                   ans+=" ";
+                      ans+=s.substr(i+1,j-i);
+               } 
+               
+         }
+        return ans;
     }
 };
