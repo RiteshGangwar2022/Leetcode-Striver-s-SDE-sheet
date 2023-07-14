f138cf9aa605a001206ee9635d991abf3afc3aa5
Ritesh Gangwar <116111183+RiteshGangwar2022@users.noreply.github.com>
2023-07-14 14:12:02 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (48.93%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,39 +1,60 @@
 class Solution {
 public:
     int myAtoi(string s) {
-             if(s.length() == 0){
-            return 0;
-        }
-
-        int i = 0;
-        while(i<s.length() && s[i] == ' '){
-            i++;
-        }
-
-        s = s.substr(i); 
-        int sign = 1;
-        long res = 0;
-
-        if(s[0] == '-'){
-            sign = -1;
-        }
+            
         
-        i = (s[0]=='+' || s[0]=='-') ? 1 : 0;
-        while(i < s.length()){
-            if(s[0] == ' ' || !isdigit(s[i])){
-                break;
-            }
-
-            res = res * 10 + s[i] - '0';
-            if(sign == -1 && -1*res <= INT_MIN){
-                return INT_MIN;
-            }
-            if(sign == 1 && res > INT_MAX){
-                return INT_MAX;
-            }
-            i++;
-        }
-
-        return res * sign;
+           if(s.size()==0){
+               return 0;
+           }
+        
+           //remove all the trailing zeros
+           int i=0;
+           while(i<s.size() && s[i]==' '){
+                i++; //increase till you find leading zeros
+           }
+        
+           //now, we will get string without leading spaces using substr function
+           s=s.substr(i);  //it will string from index i to s.size()
+         
+           long res=0;
+        
+        //now, mainting the value of positive or negative
+            int sign=1;
+           if(s[0]=='-'){
+               sign=-1;
+           }
+        
+    
+         int mini=INT_MIN;
+        int maxi=INT_MAX;
+         
+         i=(s[0]=='+' || s[0]=='-')?1:0;
+           //first element   is positive or negative , then , start trversing string from 1 else from 0 
+        
+         while(i<s.size()){
+             
+             
+              if(s[0]==' ' || !isdigit(s[i])){
+                  
+                  break; //if space or current character is not a digit ,then break the loop
+              }
+             
+              res=res*10+ s[i]-'0';
+             
+              //mainting out of integer condition
+              if(sign==-1 && -1*res<=mini){
+                  return mini;
+              }
+              if(sign==1 && 1*res>=maxi){
+                  return maxi;
+              }
+             
+              i++;
+             
+             
+         }
+        //typecasting to integer and cheking if the number is negative or positve
+        return int(res*sign);
+           
     }
 };
